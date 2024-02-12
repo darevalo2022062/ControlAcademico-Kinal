1e053347402a92b9e01d16af4e6b270245620c00
David <darevalo-2022062@kinal.edu.gt>
2024-02-12 10:27:54 -0600

    Se crea el modelo 'Student'

---
+++ models/student.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose');
+
+const StudentSchema = Schema({
+    nombre: {
+        type: String,
+        required: [true, 'Este es un campo obligatorio']
+    },
+    correo: {
+        type: String,
+        required: [true, 'Este es un campo obligatorio']
+    },
+    password: {
+        type: String,
+        required: [true, 'Este es un campo obligatorio']
+    }, role: {
+        type: String,
+        required: true,
+        default: 'STUDENT_ROLE'
+    },
+    cursos: {
+        type: String
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    }
+});
+
+module.exports = model('Student', StudentSchema);
