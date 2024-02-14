1b4f8acee46deac7a6e1738e2da4b97fb9d9e105
David <darevalo-2022062@kinal.edu.gt>
2024-02-13 18:06:45 -0600

    Controller de Curse, módulo Post

---
+++ controllers/curse.controller.js
@@ -0,0 +1,12 @@
+const { response } = require('express');
+const Curse = require('../models/curse');
+
+const cursoPost = async (req, res) => {
+    const { nombre, descripcion, maestro, cantidadDeModulos, duracionTotal, fechaFinalizacion } = req.body;
+    const curse = new Curse({ nombre, descripcion, maestro, cantidadDeModulos, duracionTotal, fechaFinalizacion });
+    await curse.save();
+
+    res.status(200).json({
+        curse
+    });
+}
