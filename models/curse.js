8288230b604e533270164e9d31c208c052dab447
David <darevalo-2022062@kinal.edu.gt>
2024-02-13 18:02:35 -0600

    modelo de Curse creado

---
+++ models/curse.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require('mongoose');
+
+const CurseSchema = Schema({
+    nombre: {
+        type: String,
+        required: [true, "EL nombre del curso es obligatorio"]
+    },
+    descripcion: {
+        type: String,
+        required: [true, "La descripción del curso es obligatoria"]
+    },
+    maestro: {
+        type: String,
+        required: [true, "El maestro del curso es obligatorio"]
+    },
+    cantidadDeModulos: {
+        type: String,
+        required: [true, "La cantidad de modulos del curso es obligatoria"]
+    },
+    duracionTotal: {
+        type: String,
+        default: "No definida"
+    },
+    fechaFinalizacion: {
+        type: String,
+        required: [true, "La fecha de finalización del curso es obligatoria"]
+    }
+});
+
+module.exports = model('Curse', CurseSchema);
