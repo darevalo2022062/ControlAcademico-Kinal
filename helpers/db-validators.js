eb010f6c107ff4d4dae0cb0dd1b1e8ad26759337
David <darevalo-2022062@kinal.edu.gt>
2024-02-14 17:07:39 -0600

    Corrección de errores

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -1,4 +1,5 @@
 const Student = require('../models/student');
+const Teacher = require('../models/teacher');
 
 const studentExists = async (correo = '') => {
     const correoExistente = await Student.findOne({ correo });
@@ -7,6 +8,22 @@ const studentExists = async (correo = '') => {
     }
 }
 
+const teacherNameExists = async (nombre = '') => {
+    var teacherExiste = await Teacher.findOne({ nombre });
+    if (teacherExiste.nombre.isEmpty()) {
+        console.log("ESE MAESTRO NO EXISTE: " + teacherExiste);
+    }
+}
+
+const teacherExists = async (correo = '') => {
+    var correoExistente = await Teacher.findOne({ correo });
+    if (correoExistente) {
+        throw new Error(`El email ya fue registrado`);
+    }
+}
+
 module.exports = {
-    studentExists
+    studentExists,
+    teacherNameExists,
+    teacherExists
 }
