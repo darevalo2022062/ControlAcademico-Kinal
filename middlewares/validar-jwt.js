a1a1fcb665624e9ff0a39546d21f2bf47397cea8
David <Darevalo-2022062@kinal.edu.gt>
2024-02-16 08:11:05 -0600

    validador

---
+++ middlewares/validar-jwt.js
@@ -0,0 +1,45 @@
+const jwt = require("jsonwebtoken");
+const Teacher = require("../models/teacher");
+const Student = require("../models/student");
+
+const validarToken = async (req, res, next) => {
+    const token = global.tokenAcces;
+
+    if (!token) {
+        return res.status(401).json({
+            msg: "Primero deberias iniciar sesion",
+        });
+    }
+
+    try {
+        const { uid } = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
+
+        const teacher = await Teacher.findById(uid);
+        const student = await Student.findById(uid);
+
+        if (!teacher) {
+            if (!student) {
+                return res.status(401).json({
+                    msg: "Usuario no existe en la base de datos",
+                });
+            }
+        }
+
+        if (!teacher.estado || teacher.estado == null) {
+            if (!student.estado) {
+                return res.status(401).json({
+                    msg: "Token no válido - usuario con estado:false",
+                });
+            }
+        }
+
+        next();
+
+    } catch (e) {
+        console.log(e),
+            res.status(401).json({
+                msg: "Token no válido",
+            });
+    }
+
+}
